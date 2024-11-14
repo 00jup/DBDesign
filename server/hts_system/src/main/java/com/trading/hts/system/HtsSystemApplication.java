fb29eb9d75a3fa4c2e622918dad25ccbd2539551
00jup <jupark0369@gmail.com>
2024-11-14 22:15:44 +0900

    spring project added

---
+++ server/hts_system/src/main/java/com/trading/hts/system/HtsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.trading.hts.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtsSystemApplication.class, args);
+    }
+
+}
---
+++ server/hts_system/src/test/java/com/trading/hts/system/HtsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading.hts.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
